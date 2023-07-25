572670451727eea53f3a05017be36b50c2f15b2e
ReactItGrup <140500952+ReactItGrup@users.noreply.github.com>
2023-07-25 16:52:07 +0500

    Add files via upload

---
+++ 1. Veriables ( O'zgaruvchilar )/script.js
@@ -0,0 +1,77 @@
+// alert("Salom JavaScript");
+
+// alert = alert yordamida saytga foydalanuvchi kirganda teppadan yozuv chiqazishimiz mumkin
+
+// Shu bilan birga consoldan foydalanishni ham ko'rib o'tamiz
+
+console.log(1);
+
+console.log(2);
+
+// Birinchi darsimiz Variables ( O'zgaruvchilar )
+
+// O'zgaruvchilar o'zi nima
+
+// O'zgaruvchilar = Keyframe ( Kalit so'zlar )
+
+let ism = "Asilbek";
+
+let yosh = 17;
+
+console.log(ism, yosh);
+
+yosh = 18;
+
+console.log(yosh);
+
+const familya = "Tursunaliyev";
+
+console.log(familya);
+
+var joy = "Toshkent viloyati";
+
+joy = "Yangiyo'l shahri";
+
+console.log(joy);
+
+// Bizda 3ta O'zgaruvchilar mavjud
+
+// 1. let
+
+// 2. const
+
+// 3. var
+
+// Bularni bir-biridan farqlari
+
+// 1. let qiymatini o'zgartirishimiz mumkin va o'zgarishi mumkin bo'lgan qiymatlarga ishlatish tavsiya etiladi
+
+// 2. const qiymatini o'zgartirishni iloji yoq va shu sababdan o'zgarmas qiymatlarga ishlatish tavsiya etiladi
+
+// 3 var hozirda ko'p ishlatilmaydi bunga sabab uni eskirib qolganligi va loyihada ubilan chiqishi mumkin bo'lgan baglar
+
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// O'zgaruvchilarga to'g'ri nom berish
+
+// let 1yosh = 17; bu noto'g'ri
+
+// let yosh1 = 17; bu to'g'ri
+
+// let - + ^ & * % @yosh = 17; bu noto'g'ri
+
+// Bular to'g'ri
+
+// let _yosh = 17;
+
+// let yosh_yoshim = 17;
+
+// let $yosh = 17;
+
+let meningIsmim = "Asilbek";
+
+// boshi kichkina va ikkinchi so'z katta bilan boshlansa Camle case ( Tuya O'rkachi ) deyiladi
+
+console.log(meningIsmim);
